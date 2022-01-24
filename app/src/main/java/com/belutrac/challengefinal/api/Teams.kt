7d12078c3aae9ae40507793e2b5892fc46b5802f
Belen Tracchia <48957384+BeluTrac@users.noreply.github.com>
2022-01-24 19:11:25 +0100

    Teams class created.

---
+++ app/src/main/java/com/belutrac/challengefinal/api/Teams.kt
@@ -0,0 +1,4 @@
+package com.belutrac.challengefinal.api
+
+class Teams(var strTeam: String, var strAlternate: String, var strStadium: String, var strStadiumLocation: String, var intStadiumCapacity : String,var strTeamBadge: String, var strWebsite : String ) {
+}
