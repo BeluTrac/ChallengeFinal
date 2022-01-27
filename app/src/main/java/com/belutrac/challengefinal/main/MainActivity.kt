2629ef5bc08be27a556b3b2e6d04b94b3695fc03
Belen Tracchia <48957384+BeluTrac@users.noreply.github.com>
2022-01-27 02:54:52 +0100

    Log out menu created

--- app/src/main/java/com/belutrac/challengefinal/main/MainActivity.kt
+++ app/src/main/java/com/belutrac/challengefinal/main/MainActivity.kt
@@ -1,20 +1,28 @@
 package com.belutrac.challengefinal.main
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
+import android.view.Menu
+import android.view.MenuItem
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
 import com.belutrac.challengefinal.R
 import com.belutrac.challengefinal.databinding.ActivityMainBinding
 import com.belutrac.challengefinal.favorites.FavFragmentDirections
+import com.belutrac.challengefinal.login.LoginActivity
+import com.belutrac.challengefinal.login.LoginViewModel
 import com.google.android.material.tabs.TabLayout
 
 class MainActivity : AppCompatActivity(){
+    private lateinit var loginViewModel : LoginViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         val tab = binding.tabLayout
+        loginViewModel = ViewModelProvider(this).get(LoginViewModel::class.java)
         tab.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
             override fun onTabSelected(tab: TabLayout.Tab) {
                 val tabItem = tab.position
@@ -34,6 +42,20 @@ class MainActivity : AppCompatActivity(){
             override fun onTabUnselected(tab: TabLayout.Tab) {}
             override fun onTabReselected(tab: TabLayout.Tab) {}
         })
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem) : Boolean{
+        if(item.itemId == R.id.log_out){
+            loginViewModel.logout()
+            startActivity(Intent(this,LoginActivity::class.java))
+        }
+        return super.onOptionsItemSelected(item)
     }
 
 }
