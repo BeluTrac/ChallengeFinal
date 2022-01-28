edeceea8df476719d2863147e5d96056ab067dab
Belen Tracchia <48957384+BeluTrac@users.noreply.github.com>
2022-01-28 15:13:22 +0100

    Delete searchView in favorite fragment and map fragment. Refactor code and fix warnings

--- app/src/main/java/com/belutrac/challengefinal/map/MapsFragment.kt
+++ app/src/main/java/com/belutrac/challengefinal/map/MapsFragment.kt
@@ -2,39 +2,30 @@ package com.belutrac.challengefinal.map
 
 import androidx.fragment.app.Fragment
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import com.belutrac.challengefinal.R
-import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
-import android.location.Geocoder
-import androidx.lifecycle.Observer
+import android.view.*
 import androidx.lifecycle.ViewModelProvider
-import com.google.android.gms.maps.GoogleMap
 
 class MapsFragment : Fragment() {
 
-    private lateinit var viewModel : MapViewModel
-    private lateinit var latlngList : MutableList<LatLng>
+    private lateinit var viewModel: MapViewModel
     private val callback = OnMapReadyCallback { googleMap ->
 
         val itemMap = viewModel.itemMapLiveData.value
 
         itemMap?.run {
             val marker = MarkerOptions().position(itemMap.latlng).title(itemMap.team.name)
-            if(itemMap.team.stadiumName.isNotBlank())
-            {
+            if (itemMap.team.stadiumName.isNotBlank()) {
                 marker.snippet(itemMap.team.stadiumName)
             }
             googleMap.addMarker(marker)
         }
 
-        if(viewModel.teamsList.value?.size!! > 1) {
-                viewModel.updateMap()
+        if (viewModel.teamsList.value?.size!! > 1) {
+            viewModel.updateMap()
         }
 
     }
@@ -45,7 +36,7 @@ class MapsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         viewModel = ViewModelProvider(this)[MapViewModel::class.java]
-
+        setHasOptionsMenu(true)
         return inflater.inflate(R.layout.fragment_maps, container, false)
     }
 
@@ -54,14 +45,20 @@ class MapsFragment : Fragment() {
         val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
 
 
-        viewModel.itemMapLiveData.observe(requireActivity()){
+        viewModel.itemMapLiveData.observe(requireActivity()) {
             mapFragment?.getMapAsync(callback)
         }
 
-        viewModel.teamsList.observe(requireActivity()){
+        viewModel.teamsList.observe(requireActivity()) {
             viewModel.updateMap()
         }
 
-        }
     }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        menu.removeItem(R.id.search_view)
+    }
+
+}
 
